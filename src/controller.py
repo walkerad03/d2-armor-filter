a483f96fd7bc3122a2b07f745f8856d28e577a1d
walkerad03 <walker@wkeithdavis.com>
2025-04-16 13:18:04 -0400

    Added logic to deal with case where there is no armor to delete

--- src/controller.py
+++ src/controller.py
@@ -2,7 +2,6 @@ import os
 
 import polars as pl
 from PyQt5.QtCore import QThreadPool
-from PyQt5.QtGui import QPixmap
 
 from src.destiny_api import ManifestBrowser
 from src.ui import AppUI, HoverImage
@@ -34,11 +33,7 @@ class AppController:
         if not self.text_result:
             self.ui.show_warning(title="Error", body="Run the filter first")
         self.ui.set_clipboard_contents(self.text_result)
-        self.ui.output_box.setText(
-            "DIM query copied to clipboard."
-        )
-
-
+        self.ui.output_box.setText("DIM query copied to clipboard.")
 
     def handle_upload(self, filepath):
         self.ui.output_box.setText(f"File selected: {filepath}")
@@ -48,14 +43,16 @@ class AppController:
         if not self.filepath:
             self.ui.show_warning(title="Error", body="No file selected.")
             return
-        
+
         self.ui.set_process_enabled_state(False)
-        
+
         self.ui.clear_photo_grid()
         self.image_placeholders = {}
-        
+
         self.df = pl.read_csv(self.filepath)
-        self.text_result, trash_armor_df = self.do_calculations(self.df, min_quality, bottom_stat_target, distributions)
+        self.text_result, trash_armor_df = self.do_calculations(
+            self.df, min_quality, bottom_stat_target, distributions
+        )
         self.ui.set_clipboard_contents(self.text_result)
 
         hash_list = trash_armor_df["Hash"].to_list()
@@ -65,6 +62,11 @@ class AppController:
         self.hash_list = hash_list
         unique_hashes = list(set(hash_list))
         self.remaining_downloads = len(unique_hashes)
+
+        if len(unique_hashes) == 0:
+            self.ui.set_process_enabled_state(True)
+            self.ui.output_box.setText("There are no armor pieces to delete!")
+            return
 
         skeleton_path = "src/assets/placeholder.png"
 
@@ -80,7 +82,7 @@ class AppController:
                 overlay_pixmap_path=None,
                 image_size=64,
                 tooltip_title="Loading...",
-                tooltip_body="Fetching item details..."
+                tooltip_body="Fetching item details...",
             )
 
             self.ui.image_box.addWidget(placeholder, row, col)
@@ -92,9 +94,10 @@ class AppController:
             task = IconLoaderRunnable(hash_value, self.api)
             task.signals.finished.connect(self._on_runner_finished)
             self.thread_pool.start(task)
-        
 
-    def do_calculations(self, armor_file, min_quality, bottom_stat_target, distributions):
+    def do_calculations(
+        self, armor_file, min_quality, bottom_stat_target, distributions
+    ):
         DEFAULT_MINIMUM_QUALITY = float(min_quality)
         DEFAULT_BOTTOM_STAT_TARGET = int(bottom_stat_target)
         IGNORE_TAGS = self.configur.getboolean("values", "IGNORE_TAGS")
@@ -117,9 +120,13 @@ class AppController:
             ALWAYS_KEEP_HIGHEST_POWER,
         )
 
-        raid_armor = self.armor_cleaner.find_raid_armor(scored_armor, DEFAULT_MINIMUM_QUALITY)
+        raid_armor = self.armor_cleaner.find_raid_armor(
+            scored_armor, DEFAULT_MINIMUM_QUALITY
+        )
 
-        low_quality_armor = self.armor_cleaner.find_low_quality_armor(scored_armor, DEFAULT_MINIMUM_QUALITY)
+        low_quality_armor = self.armor_cleaner.find_low_quality_armor(
+            scored_armor, DEFAULT_MINIMUM_QUALITY
+        )
 
         artifice_armor = self.armor_cleaner.find_artifice_armor(
             armor_file,
@@ -132,7 +139,9 @@ class AppController:
             ALWAYS_KEEP_HIGHEST_POWER,
         )
 
-        exotic_armor = self.armor_cleaner.find_exotics(scored_armor, DEFAULT_MINIMUM_QUALITY)
+        exotic_armor = self.armor_cleaner.find_exotics(
+            scored_armor, DEFAULT_MINIMUM_QUALITY
+        )
 
         class_items = self.armor_cleaner.find_class_items(armor_file)
 
@@ -140,25 +149,29 @@ class AppController:
             [low_quality_armor, raid_armor, artifice_armor, exotic_armor, class_items]
         )
 
-        armor_list = " or ".join([f"id:{item}" for item in armor_to_delete["Id"].to_list()])
+        armor_list = " or ".join(
+            [f"id:{item}" for item in armor_to_delete["Id"].to_list()]
+        )
 
         return armor_list, armor_to_delete["Id", "Hash"]
-    
+
     def get_armor_stats(self, armor_id: str) -> str:
         row = self.df.filter(pl.col("Id") == armor_id)
 
         if row.is_empty():
             return "Stats not found."
 
-        stats = row.select([
-            pl.col("Mobility"),
-            pl.col("Resilience"),
-            pl.col("Recovery"),
-            pl.col("Discipline"),
-            pl.col("Intellect"),
-            pl.col("Strength"),
-            pl.col("Total")
-        ]).to_dicts()[0]
+        stats = row.select(
+            [
+                pl.col("Mobility"),
+                pl.col("Resilience"),
+                pl.col("Recovery"),
+                pl.col("Discipline"),
+                pl.col("Intellect"),
+                pl.col("Strength"),
+                pl.col("Total"),
+            ]
+        ).to_dicts()[0]
 
         return (
             "<table style='font-family: sans-serif; font-size: 10pt;'>"
@@ -184,9 +197,7 @@ class AppController:
 
     def _handle_item_loaded(self, image_path, overlay_path, item_data):
         self.ui.add_to_photo_grid(
-            image_path=image_path,
-            overlay_path=overlay_path,
-            item_data=item_data
+            image_path=image_path, overlay_path=overlay_path, item_data=item_data
         )
 
     def _on_runner_finished(self, hash_value):
@@ -211,7 +222,7 @@ class AppController:
                     tooltip_title=item_data["name"],
                     tooltip_body=item_data["flavorText"],
                     tooltip_stats=stats_block,
-                    armor_id=armor_id
+                    armor_id=armor_id,
                 )
                 self.ui.image_box.replaceWidget(label, new_label)
                 label.deleteLater()
