cce5044c223ee745f80a1661fa93f1438011c7fb
walkerad03 <walker@wkeithdavis.com>
2025-05-16 19:31:12 -0400

    Updated bat file to install uv if not downloaded

--- run.bat
+++ run.bat
@@ -1,1 +1,11 @@
+@echo off
+
+where uv >nul 2>nul
+if %ERRORLEVEL%==0 (
+    echo uv is installed
+) else (
+    echo uv is not installed
+    powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+)
+
 uv run main.py
